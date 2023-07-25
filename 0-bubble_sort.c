3535bfaa083be0b7ce25841b1ceb995c9816e793
Yassine Rakibi <yassine@arcraven.com>
2023-07-25 09:57:23 +0200

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - func to sort array using bubble algo
+ * @array: array to be sorted
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, i = 0;
+	int aux;
+
+	if (size <= 1)
+		return;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (array[j + 1] < array[j])
+			{
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
+			j++;
+		}
+		i++;
+	}
+}
