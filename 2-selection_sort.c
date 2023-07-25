ab2f99c9d77e71844b6fd4279da01413872b19d6
Yassine Rakibi <yassine@arcraven.com>
2023-07-25 10:39:35 +0200

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - func to sort array using selection algo
+ * @array: array to be sorted
+ * @size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j, k;
+	int aux;
+
+	if (size < 2 || !array)
+		return;
+	while (i < size)
+	{
+		j = i;
+		for (k = i + 1; k < size;)
+		{
+			if (array[j] > array[k])
+				k = j;
+			j++;
+		}
+		if (j != 1)
+		{
+			aux = array[j];
+			array[j] = array[i];
+			array[i] = aux;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
